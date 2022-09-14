9f0070a80b73ff1db0a15579c2630dfe5217cad7
usylvie12 <uwasylvie12@outlook.com>
2022-09-14 23:53:41 +0200

    add tailwind task

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/*.html"],
+  theme: {
+    extend: {},
+  },
+variants:{
+  
+  extend:{
+    backgroundColor:['active'],
+  },
+},
+
+  plugins: [],
+}
